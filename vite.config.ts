7ddb25adc11af1857b526cd353852c1dbfdf9408
dseum <dseum.me@gmail.com>
2025-05-06 16:59:23 -0400

    fix: enhancedImages plugin before sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 import { enhancedImages } from '@sveltejs/enhanced-img'
 
 export default defineConfig({
-  plugins: [sveltekit(), tailwindcss(), enhancedImages()],
+  plugins: [enhancedImages(), tailwindcss(), sveltekit()],
 })
