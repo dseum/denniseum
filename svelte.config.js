ebbf40a7bc2c46a43ba2b476661bce45a2cc1749
dseum <dseum.me@gmail.com>
2025-06-08 23:36:50 +0900

    feat: cloudflare pages to workers

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import adapter from '@sveltejs/adapter-static'
+import adapter from '@sveltejs/adapter-cloudflare'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      fallback: 'plaintext',
+    }),
   },
 }
 
